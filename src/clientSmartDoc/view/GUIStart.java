4160764e7e8820d1c5c3f169f0b6d3be4dea77e9
masterdisk <costeaeduardnic@gmail.com>
2018-05-01 22:22:54 +0200

    SmartDoc 1.1 Updated GUI
    +Added JFoenix ExternalLibrary for JavaFX styling
    +Created again from scratch

--- src/clientSmartDoc/view/GUIStart.java
+++ src/clientSmartDoc/view/GUIStart.java
@@ -15,6 +15,7 @@ public class GUIStart extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
